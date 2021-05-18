192ad821617db5277c5f3773ce029613a38b0650
Conor C <CBJCalam@gmail.com>
2021-05-18 16:01:10 +1000

    Initial Project Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    /*
+    * * * * * * * * * * TO DO * * * * * * * * * *
+    * - Implement gravity for CharacterController.
+    * 
+    * - Implement and update conditions for player
+    *   death.
+    *   
+    * - Implement conditions for Respawning and
+    *   Checkpoint assignment.
+    *   
+    * - Implement PlayerState transitions and 
+    *   values.
+    * * * * * * * * * * * * * * * * * * * * * * *
+    * * * * * * OTHER SCRIPTS/OBJECTS * * * * * *
+    * - Create and implement spellbooks.
+    *       - Picking them up.
+    *       - Throwing them. 
+    *       - Casting spells. 
+    * * * * * * * * * * * * * * * * * * * * * * *
+    */
+
+    //Enum declaration for the player's current action.
+    public enum PlayerCurrentState
+    {
+        Idle,
+        Moving,
+        Falling,
+        Floating,
+        Casting,
+        Dead,
+        Respawning
+    };
+
+    //Enum declaration for the player's current spellbook element (or lack thereof, if they have no book currently)
+    public enum PlayerCurrentElement
+    {
+        None,
+        Fire,
+        Frost,
+        Earth,
+        Wind
+    };
+
+
+    [Header("PLAYER INDEX - 0 for P1, 1 for P2")]
+    public int playerIndex = 0;
+
+    [Header("Current Player State")]
+    public PlayerCurrentState playerState = PlayerCurrentState.Idle;
+
+    [Header("Character Control Variables")]
+    private float currentMoveSpeed; //Private value for current movement speed, is changed based on player state.
+    public float defaultMoveSpeed;
+    public float airborneMoveSpeed;
+    public float rotateSpeed;
+
+    //Boolean used for checking when the player is currently touching the floor.
+    private bool isGrounded;
+
+    //Variable for the CharacterController component on the object the script is attached to.
+    private CharacterController controller;
+
+    //Variables for recieving Player Inputs, and determining a movement direction vector from them.
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector2 inputVector = Vector2.zero;
+
+    //Variable for the camera used in each level.
+    private Camera levelCamera;
+
+    private void Awake()
+    {
+        //Finds the main camera on the level, used for movement and rotation directions.
+        levelCamera = Camera.main;
+
+        //Finds the Character Controller component on the object this script is attached to.
+        controller = GetComponent<CharacterController>();
+    }
+
+    public int GetPlayerIndex()
+    {
+        //Returns the playerIndex value assigned in the inspector when called. Currently used to prevent multiple input devices controlling the same character.
+        return playerIndex;
+    }
+
+    public void SetInputVector(Vector2 direction)
+    {
+        //Called by PlayerInputHandler to assign a direction for the player to move and/or face.
+        inputVector = direction;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Sets a moveDirection variable using the X and Y values of the inputVector and translating them to the X and Z values of a Vector3.
+        moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+        //Adjusts the moveDirection based on the angle of the Main Camera object.
+        moveDirection = Quaternion.Euler(0, levelCamera.gameObject.transform.eulerAngles.y, 0) * moveDirection;
+        //Lastly, multiplies moveDirection by moveSpeed to get a final value for the Controller's Move() method.
+        moveDirection *= currentMoveSpeed;
+        //Calls the Move() method on the CharacterController using the moveDirection value.
+        controller.Move(moveDirection * Time.deltaTime);
+        
+        //Translates moveDirection into a Quaternion and assigns it to the rotation variable.
+        var rotation = Quaternion.LookRotation(moveDirection);
+
+        //Checks if there is still input coming from the player. This prevents the mesh from rotating back to Y = 0 when there's no input.
+        if (moveDirection != new Vector3(0,0,0))
+        {
+            RotateTowardsMovement(rotation);
+        }
+        
+    }
+
+    public void RotateTowardsMovement(Quaternion rotation)
+    {
+        //Rotates the Player to face the value of the rotation variable.
+        gameObject.transform.rotation = Quaternion.RotateTowards(gameObject.transform.rotation, rotation, rotateSpeed);
+    }
+}
+
+
---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    //Variables grabbed on Awake that allow input to be recieved by the script, and send actions and instructions to their assigned player object.
+    private PlayerInput playerInput;
+    private PlayerController playerController;
+
+    private void Awake()
+    {
+        //Gets the Player Input component on the object this script is attatched to.
+        playerInput = GetComponent<PlayerInput>();
+        //Gets the Player Index from playerInput.
+        var index = playerInput.playerIndex;
+
+        //Finds all objects with PlayerControllers, and then assigns the object with this script to the one that shares an identical playerIndex.
+        //This prevents multiple players from somehow controlling the same player.
+        var playerControllers = FindObjectsOfType<PlayerController>();
+        playerController = playerControllers.FirstOrDefault(m => m.GetPlayerIndex() == index);
+    }
+
+    public void OnMove(CallbackContext context)
+    {
+        //Sends the Vector2 of the gamepad input to the playerController so that it can move the player.
+        if (playerController != null) //All player input methods need to check if playerController is NOT null otherwise Input System spits out a bunch of errors.
+        {
+            playerController.SetInputVector(context.ReadValue<Vector2>());
+        }
+    }
+
+}
