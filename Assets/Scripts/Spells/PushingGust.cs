34ed962dea3b06d6ab0afcfcc2a4b8a1d0d12960
Declan <declanrogers2420@gmail.com>
2021-08-10 17:35:42 +1000

    Added Gust functionality to Scones

---
+++ Assets/Scripts/Spells/PushingGust.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushingGust : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Lightable"))
+        {
+            other.gameObject.GetComponent<Scone>().isActivated = false;
+        }
+
+        if (other.CompareTag("Heavy Object"))
+        {
+
+        }
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<CharacterController>().Move((transform.position- other.gameObject.transform.position).normalized * 10);
+
+        }
+    }
+}
