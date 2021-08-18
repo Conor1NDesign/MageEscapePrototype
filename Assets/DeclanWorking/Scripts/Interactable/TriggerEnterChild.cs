049af24899861f2cd358d7b08ed84c6940d0d7bb
Declan <declanrogers2420@gmail.com>
2021-08-18 16:34:39 +1000

    Tornado mostly done

--- Assets/DeclanWorking/Scripts/Interactable/TriggerEnterChild.cs
+++ Assets/DeclanWorking/Scripts/Interactable/TriggerEnterChild.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class TriggerEnterChild : MonoBehaviour
 {
     public bool parentPlayer = true;
-    
+
     public List<PlayerController> cc;
-    Vector3 velocity;
+    public List<GameObject> Children;
     // Start is called before the first frame update
+    public Vector3 linearVel;
+
+    private Vector3 previousPosition = Vector3.zero;
     void Start()
     {
-
+        cc = new List<PlayerController>();
+        Children = new List<GameObject>();
     }
-    public Vector3 linearVel;
-    Vector3 prevPos;
-    float prevRealTime;
-    private Vector3 previousPosition = Vector3.zero;
-    private Quaternion previousRotation = Quaternion.identity;
-    bool moving;
+
+
     // Update is called once per frame
     void Update()
     {
@@ -32,42 +32,99 @@ public class TriggerEnterChild : MonoBehaviour
 
         foreach (var item in cc)
         {
-            Debug.Log(linearVel);
+            //Debug.Log(linearVel);
             item.Move((linearVel + item.moveDirection));
-            print(linearVel + item.moveDirection);
+            // print(linearVel + item.moveDirection);
         }
 
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player" && parentPlayer)
+        try
         {
-            other.gameObject.transform.parent = gameObject.transform;
-            cc.Add(other.gameObject.GetComponent<PlayerController>());
+            if (!other.CompareTag("Spellbook"))
+            {
+                if (other.tag == "Player" && parentPlayer)
+                {
+                    other.gameObject.transform.parent = gameObject.transform;
+                    cc.Add(other.gameObject.GetComponent<PlayerController>());
+                    if (!Children.Contains(other.gameObject))
+                    {
+                        Children.Add(other.gameObject);
+                    }
 
+                }
+
+                if (other.tag == "Heavy Object" && !parentPlayer)
+                {
+                    print("parent");
+                    other.gameObject.transform.parent = gameObject.transform;
+                    if (!Children.Contains(other.gameObject))
+                    {
+                        Children.Add(other.gameObject);
+                    }
+                }
+            }
         }
-
-        if (other.tag == "Heavy Object" && !parentPlayer)
+        catch
         {
-            print("parent");
-            other.gameObject.transform.parent = gameObject.transform;
+            print("q");
+
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
+        try
         {
-            other.gameObject.transform.parent = null;
-            cc.Remove(other.gameObject.GetComponent<PlayerController>());
-          
+
+            if (!other.CompareTag("Spellbook") && !other.isTrigger)
+            {
+                if (other.tag == "Player")
+                {
+                    other.gameObject.transform.parent = null;
+                    cc.Remove(other.gameObject.GetComponent<PlayerController>());
+                    Children.Remove(other.gameObject);
+
+                }
+                if (other.tag == "Heavy Object" && !parentPlayer)
+                {
+                    Children.Remove(other.gameObject);
+
+                }
+            }
+
+
         }
-        if (other.tag == "Heavy Object" && !parentPlayer)
+        catch
         {
-            print("no parent");
-            other.gameObject.transform.parent = null;
+            print("qq");
         }
     }
 
 
+    private void OnDestroy()
+    {
+        try
+        {
+            print("DESTROY");
+            foreach (var item in Children)
+            {
+                if (item.tag == "Heavy Object" || item.tag == "Heavy Object")
+                {
+                    print(item.name);
+                    item.gameObject.transform.parent = null;
+                }
+            }
+        }
+        catch
+        {
+            print("qqq");
+        }
+    }
+    private void OnDisable()
+    {
+        cc.Clear();
+        enabled = true;
+    }
 }
--- Assets/DeclanWorking/SpellCharacterController.cs
+++ Assets/DeclanWorking/SpellCharacterController.cs
@@ -6,7 +6,6 @@ using UnityEngine.InputSystem;
 public class SpellCharacterController : MonoBehaviour
 {
     CharacterController spell;
-   // [HideInInspector]
     public PlayerController PlayerCon;
     public GameObject Player;
     
@@ -15,18 +14,15 @@ public class SpellCharacterController : MonoBehaviour
     {
         //print(transform.position);
         spell = GetComponent<CharacterController>();
-        Player = GameObject.Find("Player 1");
-        print("start: " + transform.position);
-        transform.position = Player.transform.position;
+
     }
     private void Update()
     {
         Vector3 movDir = new Vector3(PlayerCon.moveDirection.x, 0.0f, PlayerCon.moveDirection.z);
-        //print("update: " + transform.position +" "+ "movdir: " + movDir);
-        print("before move" + transform.position);
+
         spell.Move(movDir * Time.deltaTime);
-        print("After move" + transform.position);
     }
+
 
 }
 
