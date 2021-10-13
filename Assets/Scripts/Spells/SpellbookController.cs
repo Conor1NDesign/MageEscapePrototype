210acdf267cc71591aa8ecf91306eb47c21f6658
Power <lerossignol.erelyn@gmail.com>
2021-10-13 16:41:20 +1100

    Fixed book destroyed by damaged or slowed blades

--- Assets/Scripts/Spells/SpellbookController.cs
+++ Assets/Scripts/Spells/SpellbookController.cs
@@ -33,13 +33,6 @@ public class SpellbookController : MonoBehaviour
 		}
 	}
 
-	void OnTriggerEnter(Collider other)
-	{
-		SpinningBlade sb = other.GetComponent<SpinningBlade>();
-		if (sb)
-			Respawn();
-	}
-
 	public void Respawn()
 	{
 		if (playerHolding)
