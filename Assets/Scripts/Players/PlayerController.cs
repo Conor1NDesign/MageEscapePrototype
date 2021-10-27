c924887f6ed41b657fa73e7d52f8039e81a3c9c0
Power <lerossignol.erelyn@gmail.com>
2021-10-27 16:16:36 +1100

    Oops

--- Assets/Scripts/Players/PlayerController.cs
+++ Assets/Scripts/Players/PlayerController.cs
@@ -482,7 +482,7 @@ public class PlayerController : MonoBehaviour
 			playerElement = spellbook.GetComponent<SpellbookController>().element;
 
 			spellbook.transform.parent = spellbookEquipPoint;
-			spellbook.transform.position = new Vector3();//spellbookEquipPoint.position;
+			spellbook.transform.position = spellbookEquipPoint.position;
 			spellbook.GetComponent<Rigidbody>().isKinematic = true;
 			spellbook.GetComponent<SpellbookController>().playerHolding = this;
 		}
