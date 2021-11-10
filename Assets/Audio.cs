03c417e35e48894d83c1c14871d00384bd39f9b8
Declan <declanrogers2420@gmail.com>
2021-11-10 15:17:00 +1100

    Options Sound setting added

---
+++ Assets/Audio.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+
+    public AudioSource[] audio;
+    public float volume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = FindObjectsOfType<AudioSource>();
+        volume = PlayerPrefs.GetFloat("Sound");
+        foreach (var item in audio)
+        {
+            item.volume = PlayerPrefs.GetFloat("Sound");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerPrefs.GetFloat("Sound") != volume)
+        {
+            UpdateSound();
+            volume = PlayerPrefs.GetFloat("Sound");
+        }
+    }
+
+
+    public void UpdateSound()
+    {
+        print("Sounds Updated");
+        foreach (var item in audio)
+        {
+            item.volume = PlayerPrefs.GetFloat("Sound");
+        }
+    }
+}
---
+++ Assets/Options.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Slider slider;
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("Sound");
+    }
+
+    // Update is called once per frame
+
+
+    public void OnDrag()
+    {
+        PlayerPrefs.SetFloat("Sound", slider.value);
+    }
+}
