c866db899eb0b68376cf15250cbcad60ce66bb36
Conor C <CBJCalam@gmail.com>
2021-11-11 15:38:11 +1100

    Intro Cinematic script that moves to the next scene when video ends

---
+++ Assets/Scripts/GameSystems/CinematicSceneTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class CinematicSceneTrigger : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.loopPointReached += LoadScene;
+    }
+
+    public void LoadScene(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
